85acb1f3df732dd66670324db589007e9ea43eec
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-07 22:38:01 +0200

    Add books grid container and setup displaying books functionality

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const myLibrary = [];
+const myLibrary = [new Book('The Hobbit', 'J.R.R', 300, true)];
+const booksContainer = document.getElementsByClassName('books-container')[0];
 
-function Book(title, author, isRead) {
+function Book(title, author, numberOfPages, isRead) {
   this.title = title;
   this.author = author;
+  this.numberOfPages = numberOfPages;
   this.isRead = isRead;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
   myLibrary.push(new Book());
 }
 
 function displayBooks() {
-  myLibrary.forEach((book) => {});
+  myLibrary.forEach((book) => {
+    booksContainer.appendChild(BookCard(book));
+  });
 }
+
+function BookCard(book) {
+  const bookCard = document.createElement('div');
+  bookCard.className = 'book-card';
+  bookCard.innerHTML = `<span class="name">${book.title}</span>
+			<span class="author-name">${book.author}</span>
+			<span class="number-of-pages">Pages: ${book.numberOfPages}</span>
+			<div class="read-check-container">
+				<input type="checkbox" name="isRead" id="read-check">
+				<label for="read-check">Read</label>
+			</div>`;
+  return bookCard;
+}
+
+displayBooks();
